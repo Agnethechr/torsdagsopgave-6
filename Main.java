48f37e2372f67a183645d29ed4aaffcebf9c5d14
Agnethechr <144229376+Agnethechr@users.noreply.github.com>
2024-03-15 10:29:30 +0100

    Main3.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Main {
+public class Main3 {
     public static void main(String[] args) {
 
         ArrayList<String> passedCourses = new ArrayList<>();
