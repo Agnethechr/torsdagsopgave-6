4635eaf8960425d18674f6b9142c70f8cff64a24
Agnethechr <144229376+Agnethechr@users.noreply.github.com>
2024-03-15 10:25:05 +0100

    Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+
 public class Teacher extends Person{
-    public Teacher(String name) {
+
+    ArrayList<String> canTeach = new ArrayList<>();
+    ArrayList<String> currentCourses = new ArrayList<>();
+    public Teacher(String name, ArrayList<String> canTeach) {
         super(name);
+        this.canTeach = canTeach;
+    }
+
+    public boolean addCourse(String course){
+        if(canTeach.contains(course)) {
+        return false;
+        }else{
+            canTeach.add(course);
+            return true;
+        }
     }
 }
