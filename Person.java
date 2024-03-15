0b1636dda4ac5f419bc5862df6cc9e9c19c00187
Agnethechr <144229376+Agnethechr@users.noreply.github.com>
2024-03-15 10:25:47 +0100

    Person.java

--- Person.java
+++ Person.java
@@ -3,10 +3,15 @@ public class Person {
     String name;
 
     public Person(String name) {
+        this.name=name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(){
+        this.name=name;
     }
 
     public boolean addCourse(String course){
